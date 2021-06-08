2674eccbb918330ec38c26e538ee66cf4d461182
Awongosakethi <56240483+Awongosakethi@users.noreply.github.com>
2021-06-08 13:31:13 +0700

    Add files via upload

---
+++ lab9_php_pertanyaan&tugas/about.php
@@ -0,0 +1,10 @@
+<?php require('header.php'); ?>
+
+<div class="content">
+    <h2>About Us</h2>
+    <br>
+    <p>Assalamualaikum... 
+    <br>Saya Awong Osakethi mahasiswa Universitas Pelita Bangsa</p>
+</div>
+
+<?php require('footer.php'); ?>
---
+++ lab9_php_pertanyaan&tugas/footer.php
@@ -0,0 +1,6 @@
+<footer>
+            <p>&copy; 2021, Awong Osakethi, 311910499, TI.19.A2</p>
+        </footer>
+    </div>
+</body>
+</html>
